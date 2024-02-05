c2aff5858c26d12fb5ab6ae782a964a085dcff35
ZeldaHatGuy <greg.k.singletary@gmail.com>
2024-02-04 20:53:25 -0600

    adding new logger version

--- logging/customLog.py
+++ logging/customLog.py
@@ -2,20 +2,27 @@ import logging
 import logging.config
 import json
 import logging.handlers
-import pathlib
 import colorlog 
+import datetime as dt
+import atexit
+import inspect
 
+
+
+logger = logging.getLogger(__name__)
 
 
 def configure_logging(stdout_enabled=True, file_enabled=True, config='logging_config.json'):
     # Load the JSON configuration
     with open('logging_config.json', 'r') as config_file:
         logging_config = json.load(config_file)
+        
+
 
     # Disable or enable stdout and file handlers based on the provided arguments
     handlers = []
     if stdout_enabled:
-        handlers.append("stderr")
+        # handlers.append("stderr")
         handlers.append("stdout_info_debug")
     if file_enabled:
         handlers.append("file")
@@ -25,26 +32,39 @@ def configure_logging(stdout_enabled=True, file_enabled=True, config='logging_co
 
     # Configure logging using dictConfig
     logging.config.dictConfig(logging_config)
+    queue_handler = logging.getHandlerByName("queue_handler")
+    if queue_handler is not None:
+        queue_handler.listener.start()
+        atexit.register(queue_handler.listener.stop)
 
 # Set up the custom SUCCESS log level
 SUCCESS = 25
 logging.addLevelName(SUCCESS, "SUCCESS")
 
+# def success(self, message, *args, **kwargs):
+#     self.log(SUCCESS, message, *args, **kwargs)
+    
+# logging.Logger.success = success
 def success(self, message, *args, **kwargs):
-    self.log(SUCCESS, message, *args, **kwargs)
+    # Get the calling function's name
+    calling_function = inspect.currentframe().f_back.f_globals.get('__name__')
+    
+    # Include the calling function's name in the log message
+    message_with_function = f"{calling_function}: {message}"
+    
+    # Log the message
+    self._log(SUCCESS, message_with_function, args, **kwargs)
 
+# Add the success method to the Logger class
 logging.Logger.success = success
 
-
-
-    
-    
-    
     
     
 def test_logger():
 
     configure_logging()
+
+ 
     logger.debug("debug message")
     logger.info("info message")
     logger.warning("warning message")
@@ -55,7 +75,7 @@ def test_logger():
     try:
         1 / 0
     except ZeroDivisionError as e:
-        logger.critical(f"recieved the follow exception: {e.args[0]}")
+        logger.critical(f"Recieved the Following Exception: {e.args[0]}")
         
         
         
